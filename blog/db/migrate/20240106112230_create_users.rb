30439be774c00c946ee0c1f4e4158c1d821a984f
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2024-01-06 20:27:16 +0900

    add User model and schema

---
+++ blog/db/migrate/20240106112230_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps
+    end
+    add_index :users, :name, unique: true
+    add_index :users, :email, unique: true
+  end
+end
