84f60ef1670a20bc5e4f04ea3fce7db5eaf39f46
Yusuke Sato ( YS office LLC Founder ) <yusuke.sato@ys-office.llc>
2024-01-06 22:39:36 +0900

    add spec for Page

--- blog/app/models/page.rb
+++ blog/app/models/page.rb
@@ -1,3 +1,6 @@
 class Page < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, uniqueness: true
+  validates :content, presence: true
 end
--- blog/spec/models/page_spec.rb
+++ blog/spec/models/page_spec.rb
@@ -1,9 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Page, type: :model do
-  let(:page) { build(:page) }
+  subject { build(:page) }
 
-  it 'has a valid factory' do
-    expect(page).to be_valid
+  describe 'validations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to be_valid }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_uniqueness_of(:title) }
+    it { is_expected.to validate_presence_of(:content) }
   end
 end
